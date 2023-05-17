81e06f46b15b339d78da52795c1d692b56b3223d
Eric VanBergen <vanberge@gmail.com>
2023-05-17 16:12:54 -0400

    update to help readability

--- permissions.sh
+++ permissions.sh
@@ -1,5 +1,8 @@
+echo "Looping through projects..."
 gcloud projects list | grep "PROJECT_ID" | awk '{print $2}' | while read PROJECT
 do
- gcloud projects get-ancestors-iam-policy $PROJECT
+	echo "Generating permissions audit for $PROJECT..."
+	echo "============================================="
+	gcloud projects get-ancestors-iam-policy $PROJECT
+	echo "    "
 done
-
